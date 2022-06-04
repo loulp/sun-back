78c9d0f68b336458fed472221f2d67ca32690347
loulp <louislpogam@orange.fr>
2022-06-04 15:13:39 +0200

    update & modify conf

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', '8d0225b3132b133dd1edf4c5cc62ee36'),
+    secret: env("ADMIN_JWT_SECRET"),
+  },
+  apiToken: {
+    salt: env("API_TOKEN_SALT"),
   },
 });
