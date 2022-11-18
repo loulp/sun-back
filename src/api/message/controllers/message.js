9e2118f3571b676d0b01883a91eaaf39de7c19b5
loulp <louislpogam@orange.fr>
2022-11-18 17:47:09 +0100

    add phone number to contact

--- src/api/message/controllers/message.js
+++ src/api/message/controllers/message.js
@@ -14,19 +14,23 @@ module.exports = createCoreController("api::message.message", ({ strapi }) => ({
       await strapi.plugins["email"].services.email.send({
         // TODO change with SUN email (dans strapi -> mail setting aussi)
         to: "s.u.n.jewelry@hotmail.com",
+        // to: "llp_dev@outlook.com",
         replyTo: ctx.request.body.email,
         subject: ctx.request.body.subject,
         text: `${ctx.request.body.message} 
         - Email envoyé depuis le formulaire de contact du site web
         `,
-        html: ctx.request.body.message,
+        html: `${ctx.request.body.message}
+        <br/>
+        <br/>
+        Numéro de téléphone : ${ctx.request.body.phoneNumber}
+        `,
       });
       ctx.response.status = 200;
     } catch (err) {
       console.log(err);
       ctx.body = err;
       ctx.response.status = 500;
-
     }
   },
 }));
